0f8a4f37889f764ccb81ac562b1e493ff78cf3cc
Kelechukwu <kelebrizzle@gmail.com>
2023-11-23 01:25:13 +0000

    mysql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
-sts all shows contained in the database hbtn_0d_tvshows
+-- lists all shows contained in the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres RIGHT JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
